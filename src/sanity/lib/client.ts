9b22533bbd6295eff27518a00a97501ee8d1ec76
Riaz zaur <riazahmedzaur110@gmail.com>
2025-01-03 23:58:02 +0500

    update

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -6,5 +6,6 @@ export const client = createClient({
   projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset : process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion,
+  token:process.env.NEXT_API_TOKEN,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
