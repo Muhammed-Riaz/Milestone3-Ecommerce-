d375aec322260be651c4ac680bc0ee347675f2c7
Riaz zaur <riazahmedzaur110@gmail.com>
2025-01-31 20:50:25 +0500

    sanity token error handle

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,9 +1,10 @@
-import { createClient } from "next-sanity";
+
+import { createClient } from "@sanity/client";
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: "2023-01-01",
-  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,  // Ensure it's correct
-  useCdn: false,  // Use false for fresh data
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET|| "production",
+  useCdn: false, // False to get fresh data
+  apiVersion: "2024-01-31", // Latest API version
+  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN, // Use API Token for Auth
 });
